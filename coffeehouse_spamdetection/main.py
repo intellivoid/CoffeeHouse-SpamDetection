186e14ec30b9a845be1312e765b15b39738c5b11
netkas <netkas@intellivoid.info>
2020-03-02 23:55:19 -0300

    Added missing methods

--- coffeehouse_spamdetection/main.py
+++ coffeehouse_spamdetection/main.py
@@ -5,10 +5,16 @@ from coffeehouse_dltc.chmodel.configuration import Configuration
 class SpamDetection(object):
 
     def __int__(self):
-        self.models_directory = os.path.join(os.path.dirname(__file__), 'model')
-        self.models_trained_directory = os.path.join(os.path.dirname(__file__), 'model_output')
         self.dltc = DLTC()
 
+    @staticmethod
+    def models_directory():
+        return os.path.join(os.path.dirname(__file__), 'model')
+
+    @staticmethod
+    def models_trained_directory():
+        return  os.path.join(os.path.dirname(__file__), 'model_output')
+
     def train_model(self):
-        configuration = Configuration(self.models_directory)
+        configuration = Configuration(self.models_directory())
         configuration.train_model()
