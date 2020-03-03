29267d50b0e0e4c287760af99c643f2afc697394
netkas <netkas@intellivoid.info>
2020-03-02 23:52:42 -0300

    Reordered statements

--- coffeehouse_spamdetection/main.py
+++ coffeehouse_spamdetection/main.py
@@ -5,9 +5,9 @@ from coffeehouse_dltc.chmodel.configuration import Configuration
 class SpamDetection(object):
 
     def __int__(self):
-        self.dltc = DLTC()
         self.models_directory = os.path.join(os.path.dirname(__file__), 'model')
         self.models_trained_directory = os.path.join(os.path.dirname(__file__), 'model_output')
+        self.dltc = DLTC()
 
     def train_model(self):
         configuration = Configuration(self.models_directory)
