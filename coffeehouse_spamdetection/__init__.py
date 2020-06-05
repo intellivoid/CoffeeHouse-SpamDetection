e7d04f8d368979915f2c48cac00225b86c6c20a5
netkas <netkas@intellivoid.info>
2020-06-05 17:56:54 -0400

    Updated help

--- coffeehouse_spamdetection/__init__.py
+++ coffeehouse_spamdetection/__init__.py
@@ -1,7 +1,4 @@
 from . import main
 from .main import *
 
-from . import __main__
-from .__main__ import *
-
-__all__ = ["__main__", "main", "SpamDetection"]
+__all__ = ["main", "SpamDetection"]
--- coffeehouse_spamdetection/__main__.py
+++ coffeehouse_spamdetection/__main__.py
@@ -1,4 +1,57 @@
-from coffeehouse_spamdetection import main
+import sys
+import os
+
+from coffeehouse_spamdetection import SpamDetection
+
+
+def _real_main(argv=None):
+    """
+    The main command-line processor
+
+    :param argv:
+    :return:
+    """
+    if argv[1] == '--help':
+        _help_menu(argv)
+    if argv[1] == '--test':
+        _test_model(argv)
+
+
+def _help_menu(argv=None):
+    """
+    Displays the help menu and commandline usage
+
+    :param argv:
+    :return:
+    """
+    print(
+        "CoffeeHouse DLTC CLI\n\n"
+        "   --model-info <directory_structure_input>\n"
+        "   --train-model <directory_structure_input>\n"
+        "   --test-model <model_directory>\n"
+    )
+    sys.exit()
+
+
+def _test_model(argv=None):
+    """
+    Tests the model's prediction by allowing user input and displaying the
+    prediction output
+
+    :param argv:
+    :return:
+    """
+    print("Loading")
+    spam_detection = SpamDetection()
+    print("Ready\n")
+
+    while True:
+        input_text = input("> ")
+        print(spam_detection.predict(input_text))
+
 
 if __name__ == '__main__':
-    print("This only executes when %s is executed rather than imported" % __file__)
+    try:
+        _real_main(sys.argv)
+    except KeyboardInterrupt:
+        print('\nERROR: Interrupted by user')
