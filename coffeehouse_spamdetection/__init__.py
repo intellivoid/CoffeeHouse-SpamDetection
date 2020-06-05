58f3d6b33b512b2d820994a70d61bb52849f6895
netkas <netkas@intellivoid.info>
2020-06-05 17:46:14 -0400

    Updated main endpoint

--- coffeehouse_spamdetection/__init__.py
+++ coffeehouse_spamdetection/__init__.py
@@ -1,4 +1,7 @@
 from . import main
 from .main import *
 
-__all__ = ["main", "SpamDetection"]
+from . import __main__
+from .__main__ import *
+
+__all__ = ["__main__", "main", "SpamDetection"]
---
+++ coffeehouse_spamdetection/__main__.py
@@ -0,0 +1,4 @@
+from coffeehouse_spamdetection import main
+
+if __name__ == '__main__':
+    print("This only executes when %s is executed rather than imported" % __file__)
