ce692907141a16e465f8ea02adb899db76850812
netkas <netkas@intellivoid.info>
2020-06-05 17:05:26 -0400

    Updated Makefile

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
-        'coffeehouse_spamdetection'
+        'coffeehouse_spamdetection',
+        'hyper_internal_service'
     ],
     entry_points='''
         [console_scripts]
